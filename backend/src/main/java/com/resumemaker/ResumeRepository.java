0c1a60554d1a4999b7782d377337483504c2e3c7
deepak042 <bapunbehera042@gmail.com>
2025-11-01 16:47:59 +0530

    add

--- backend/src/main/java/com/resumemaker/ResumeRepository.java
+++ backend/src/main/java/com/resumemaker/ResumeRepository.java
@@ -4,11 +4,10 @@ package com.resumemaker.repository;
 import com.resumemaker.model.Resume;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-// No methods are needed here!
-// Spring Data provides findById(), save(), findAll(), etc. automatically.
-// The two methods you had were:
-// 1. findById() - which is redundant.
-// 2. findByUserId() - which was unused.
+// This is the entire, correct file.
+// It should be empty inside because Spring Data provides all the methods
+// like findById(), save(), etc. automatically.
 public interface ResumeRepository extends MongoRepository<Resume, String> {
-    // This file is now clean!
+    // There should be nothing in here!
 }
+
